b493510110d4b10b1a188ccf7d4178e041b11bd0
AtayevZarif <atayev1234567890@gmail.com>
2024-06-27 22:36:46 +0500

    Succsess

---
+++ main.py
@@ -0,0 +1,7 @@
+name = "Zarif"
+surname = "Atayev"
+age = 13
+
+print(f"Mening ismim: {surname} {name}")
+print(f"Men {age} yoshda programmist bo'lganman")
+print("'Ozimning bo'sh vaqtimda, men 'oyin o'ynayman va youtube ko'raman")
